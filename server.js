a39fdc7970d4064ef6933e9145e31cc05d20ff13
Estera-Mieszczak <144910099+Estera-Mieszczak@users.noreply.github.com>
2024-07-28 10:07:55 +0200

    avatars

--- server.js
+++ server.js
@@ -1,12 +1,66 @@
 const express = require("express");
+const jimp = require("jimp");
+const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const app = express();
 const apiRouter = require("./routes/api/contacts");
+// const { required } = require("joi");
+const { setupFolder } = require("./functions/functions");
+const multer = require("multer");
+const { v4: uuidV4 } = require("uuid");
+const fs = require("fs").promises;
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(path.resolve(__dirname, "./public")));
 
 require("dotenv").config();
+
 const { DB_HOST: urlDb } = process.env;
 const connection = mongoose.connect(urlDb);
+
+const tempDir = path.join(process.cwd(), "temp");
+const storageAvatarDir = path.join(process.cwd(), "public/avatars");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, tempDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${uuidV4()}${file.originalname}`);
+  },
+});
+
+const extensionWhiteList = [".jpg", ".jpeg", ".png", ".gif"];
+const mimetypeWhiteList = ["image/png", "image/jpg", "image/jpeg", "image/gif"];
+
+const uploadMiddleware = multer({
+  storage,
+  fileFilter: async (req, file, cb) => {
+    const extension = path.extname(file.originalname).toLowerCase();
+    const mimetype = file.mimetype;
+    if (
+      !extensionWhiteList.includes(extension) ||
+      !mimetypeWhiteList.includes(mimetype)
+    ) {
+      return cb(null, false);
+    }
+    return cb(null, true);
+  },
+  limits: { fileSize: 1024 * 1024 * 5 },
+});
+
+app.post(
+  "/upload",
+  uploadMiddleware.single("avatar"),
+  async (req, res, next) => {
+    if (!req.file) {
+      return res.status(400).json({ message: "File is not a photo" });
+    }
+    res.json(req.file);
+  }
+);
 
 app.use(express.json());
 app.use(cors());
@@ -31,7 +85,9 @@ const startServer = async () => {
   try {
     await connection;
     console.log("Database connection successful");
-    app.listen(3000, () => {
+    app.listen(3000, async () => {
+      await setupFolder(tempDir);
+      await setupFolder(storageAvatarDir);
       console.log("Server running. Use our API on port: 3000");
     });
   } catch (error) {
